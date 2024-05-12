46155359141d07b383fd4025237dcad92335beeb
Dheer Kumar Sharma <137029706+DheerKumarSharmaIndia@users.noreply.github.com>
2024-05-12 18:47:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const hourHand = document.querySelector('.hour-hand');
-const minuteHand = document.querySelector('.minute-hand');
-const secondHand = document.querySelector('.second-hand');
-
 function updateClock() {
   const now = new Date();
   const hours = now.getHours() % 12;
@@ -11,6 +7,10 @@ function updateClock() {
   const hourDeg = (hours * 30) + (0.5 * minutes); // 30 degrees per hour, 0.5 degrees per minute
   const minuteDeg = (minutes * 6) + (0.1 * seconds); // 6 degrees per minute, 0.1 degrees per second
   const secondDeg = seconds * 6; // 6 degrees per second
+
+  const hourHand = document.querySelector('.hour-hand');
+  const minuteHand = document.querySelector('.minute-hand');
+  const secondHand = document.querySelector('.second-hand');
 
   hourHand.style.transform = `rotate(${hourDeg}deg)`;
   minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
